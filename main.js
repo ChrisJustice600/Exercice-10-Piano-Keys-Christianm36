e63b2c1e2b437d31ed4667bbfb034ac537366879
Mabeladi <christian.mabeladi@kadea.local>
2023-08-08 11:06:08 +0100

    last function

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ nextOne.addEventListener("click", ()=> {
   nextOne.hidden = true
   document.getElementById('letter-note-five').innerHTML = 'D';
   document.getElementById('letter-note-six').innerHTML = 'C';
-  lastLyric.style.display = "inline-block"
+  
 })
 
 // Write anonymous event handler property and function for the second progress button
@@ -75,11 +75,31 @@ nextTwo.addEventListener("click", ()=> {
   nextTwo.hidden = true
   document.getElementById('word-five').innerHTML = 'DEAR';
   document.getElementById('word-six').innerHTML = 'FRI';
-
+  document.getElementById('letter-note-three').innerHTML = 'G';
+  document.getElementById('letter-note-four').innerHTML = 'E';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('letter-note-six').innerHTML = 'B';
+  lastLyric.style.display = "inline-block"
 })
 
 // Write anonymous event handler property and function for the third progress button
-
+nextThree.addEventListener("click", ()=> {
+  startOver.hidden= false;
+  nextThree.hidden = true;
+  document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+  document.getElementById('letter-note-one').innerHTML = 'F';
+  document.getElementById('letter-note-two').innerHTML = 'F';
+  document.getElementById('letter-note-three').innerHTML = 'E';
+  document.getElementById('letter-note-four').innerHTML = 'C';
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+  lastLyric.style.display = "none"
+})
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
@@ -97,6 +117,7 @@ startOver.onclick = function() {
   document.getElementById('letter-note-five').innerHTML = 'C';
   document.getElementById('word-six').innerHTML = 'YOU!';
   document.getElementById('letter-note-six').innerHTML = 'B';
+ 
 }
 
 
